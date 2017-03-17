a9f23cf8afc85ade9e6e26da2821896e3b16d26b
panacea101 <panacea101@gmail.com>
2017-03-17 08:44:40 -0400

    Saving new changes

---
+++ ReactNativeTests/ReactNativeTests/Pages/ForgotPassword.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace ReactNativeTests
+{
+	public class ForgotPassword : Accounts
+	{
+		Query backArrowButton;
+		Query forgotPasswordLabel;
+		//Query skipButton;
+		Query enterEmailText;
+		Query emailAddressTextbox;
+		Query sendResetEmailButton;
+
+		public ForgotPassword()
+		{
+			backArrowButton = x => x.Marked("back button");
+			forgotPasswordLabel = x => x.Marked("forgot password label");
+			enterEmailText = x => x.Marked("Enter your email address below. If there is an account on Sothebys.com that matches it, instructions for resetting your password will be sent to that email address. label");
+			emailAddressTextbox = x => x.Marked("email address textinput");
+			sendResetEmailButton = x => x.Marked("send reset email button"); 
+		}
+
+		public void enterForgottenEmail()
+		{
+			app.Tap(enterEmailText);
+			app.EnterText("fakeEmail@sothebysqa.com");
+		}
+
+		public void sendResetEmail()
+		{
+			app.Tap(sendResetEmailButton);
+		}
+
+		public void tapBackButton()
+		{
+			app.Tap(backArrowButton);
+		}
+	}
+}
---
+++ ReactNativeTests/ReactNativeTests/Pages/SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;e
+
+namespace ReactNativeTests
+{
+	public class SignIn : Accounts
+	{
+		Query signInLabel;
+		Query emailAddressTextbox;
+		Query passwordTextbox;
+		Query forgotPasswordButton;
+		Query signInButton;
+		//Query orText;
+		Query createAccountButton;
+
+		public SignIn()
+		{
+			signInLabel = x => x.Marked("sign in label");
+			emailAddressTextbox = x => x.Marked("email address textinput");
+			passwordTextbox = x => x.Marked("password textinput");
+			forgotPasswordButton = x => x.Marked("forgot password button");
+			signInButton = x => x.Marked("sign in button");
+			createAccountButton = x => x.Marked("create account button");
+		}
+
+	}
+}
